71a3a2814499a750135d61bdc2b7081475935a1a
Shokhmurod <shokhpjwstk@gmail.com>
2023-01-05 15:03:10 +0100

    added api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f84ac9eaa0mshef9d164172ca7edp1f50f5jsn88dab71619c0",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export default GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";

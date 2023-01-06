fd782d2a1d1848663e647a412499bcfa30a95729
Shokhmurod <shokhpjwstk@gmail.com>
2023-01-06 11:09:32 +0100

    finished the forecast, styles, and the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
@@ -17,7 +15,6 @@ function App() {
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
-
     const forecastFetch = fetch(
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
@@ -25,15 +22,13 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
+        const forcastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forecastResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
       })
-      .catch((err) => console.log(err));
+      .catch(console.log);
   };
-
-  console.log(forecast);
 
   return (
     <div className="container">
